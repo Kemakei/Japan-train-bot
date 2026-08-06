9ebf3dd9a3adb47151e18eb81eec4eb0dda2cb3a
Kemakei <keisukemark0124@gmail.com>
2026-08-07 04:59:26 +0900

    fix

--- public/game.js
+++ public/game.js
@@ -1,6 +1,14 @@
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
 
+function resizeCanvas() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+}
+
+resizeCanvas();
+window.addEventListener("resize", resizeCanvas);
+
 let mapData;
 
 const tilesets = [];
